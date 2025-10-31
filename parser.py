9baf8e1fedf7d33d401dea5973b8c1dc809f9db7
Yusuke Matsuki <yusukematsuki123@gmail.com>
2025-10-31 18:28:26 +0800

    wip: html response structured parser

---
+++ parser.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+# References obtained from myjian's mai-tools
+
+RECORD_URL_MAP = {
+    'ReMASTER':'/record/musicGenre/search/?genre=99&diff=4',
+    'MASTER':'/record/musicGenre/search/?genre=99&diff=3',
+    'EXPERT':'/record/musicGenre/search/?genre=99&diff=2',
+    'ADVANCED': '/record/musicGenre/search/?genre=99&diff=1',
+    'BASIC':'/record/musicGenre/search/?genre=99&diff=0'
+}
