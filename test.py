5dc3cb3099e92fdf5ec71d7e606428431e3be6c0
Yusuke Matsuki <yusukematsuki123@gmail.com>
2025-11-01 11:44:14 +0800

    feat: refactoring class inheritance to avoid deprecation

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 from session import MaimaiSession, MaimaiEXSession
+from dotenv import load_dotenv
 import os
 import asyncio
-
+if not load_dotenv(): raise EnvironmentError("Failed to load .env")
 loop = asyncio.new_event_loop()
-cookie = os.getenv('COOKIE','<COOKIE>')
 
 async def simple_session_test():
+    cookie = os.getenv('COOKIE',None)
+    if not cookie:
+        raise EnvironmentError("Failed to retrieve COOKIE from .env")
+    
     mai = MaimaiEXSession(cookie=cookie)
     await mai.get_data()
     print(mai.data)
     await mai.logout("/home/userOption","/logout/?")
-    await mai.close()
+    await mai.session.close()
 
 if __name__ == "__main__":
     loop.run_until_complete(simple_session_test())
