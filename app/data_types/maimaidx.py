ac1f490223147c34e599a37fcd4923a8c704ef78
Yusuke Matsuki <yusukematsuki123@gmail.com>
2025-11-05 18:12:53 +0800

    refactor: centralised data type hints for MDX

---
+++ app/data_types/maimaidx.py
@@ -0,0 +1,26 @@
+from typing import Literal, Union
+
+MDXGameCoefficient = float
+
+MDXSongName = str
+MDXChartLevel = str
+MDXChartInternal = float
+MDXChartType = Literal['STD','DX']
+MDXChartDifficulty = Literal['ReMASTER', 'MASTER', 'EXPERT', 'ADVANCED','BASIC']
+
+MDXChartValue = Union[MDXSongName,MDXChartType,MDXChartDifficulty]
+MDXChartParams = Literal['chart_type', 'song_name', 'difficulty']
+MDXChartTuple = tuple[MDXSongName,MDXChartType,MDXChartDifficulty]
+MDXChartQueryArgs = dict[MDXChartParams,Union[MDXSongName,MDXChartDifficulty,MDXChartType]]
+MDXChartInternalMap = dict[MDXChartTuple,MDXChartInternal]
+MDXChartSheetMap =dict[MDXChartTuple,dict]
+
+MDXRecordParams = Literal['achievement','achievement_float','rank'] #TODO: DX
+MDXRecordRank = Literal['sssp','sss','ssp','ss','sp','s','aaa','aa','a','bbb','bb','b','c','d']
+MDXRecordAchievement = str
+MDXRecordAchievementFloat = float
+MDXRecordRating = int
+MDXRecordValue = Union[MDXRecordRank,MDXRecordAchievement, MDXRecordAchievementFloat, MDXRecordRating]
+
+
+MDXDataValue = Union[MDXRecordValue,MDXChartValue]
